a4e028f4485e7be6d25fa4ac5d16bf7d5dee4534
Mykola <mikolakovalchukwt@gmail.com>
2016-11-30 00:05:54 +0200

    Added trial service

---
+++ app/services/trial_service.rb
@@ -0,0 +1,27 @@
+class TrialService
+  def initialize(name, surname, lastname, product, user, age, living_place)
+    @name = name
+
+
+    @surname = surname
+
+
+    @lastname = lastname
+
+
+    @product = product
+
+
+    @user = user
+
+
+    @age = age
+
+
+    @living_place = living_place
+  end
+
+def perform!
+  p @name
+end
+end
